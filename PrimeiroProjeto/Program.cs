bf410856371a96559cbb3ad00e57bfdb0a69f432
lnascimento <lnascimento@BRCPHDLAP037>
2024-10-09 13:34:22 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExibirLogo();
+        ExibirOpcoesDoMenu();
+    }
+
+    static string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+    static List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+
+    static void ExibirLogo()
+    {
+        Console.WriteLine(@"
+
+        ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+        ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+        ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+        ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+        ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+        ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+        ");
+        Console.WriteLine(mensagemDeBoasVindas);
+    }
+    static void ExibirOpcoesDoMenu() //Mova a função para dentro da classe
+    {
+        Console.WriteLine("Digite 1 para registrar uma banda");
+        Console.WriteLine("Digite 2 para mostrar todas as bandas");
+        Console.WriteLine("Digite 3 para avaliar uma banda");
+        Console.WriteLine("Digite 4 para exibir a média de uma banda");
+        Console.WriteLine("Digite -1 para sair");
+
+        Console.Write("Digite a sua opcao: ");
+        string opcaoEscolhida = Console.ReadLine();
+        int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+
+        switch (opcaoEscolhidaNumerica)
+        {
+            case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                break;
+            case 2: Console.WriteLine("Você escolheu a opção " +  opcaoEscolhidaNumerica);
+                break;
+            case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                break;
+            case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                break;
+            case -1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                break;
+            default: Console.WriteLine("Opção Inválida");
+                break;
+        }
+    }
+}
